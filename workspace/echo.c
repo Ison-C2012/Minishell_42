059ce00f4e8a17dc49306a642d7f148e4caa6d47
ison <ison.c.2012.tk@gmail.com>
2025-12-09 19:26:21 +0900

    proto echo

--- workspace/echo.c
+++ workspace/echo.c
@@ -1,4 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NO 0
+#define YES 1
+
 int	main(int ac, char **av, char **ev)
 {
-	
+	int	nl;
+	int	i;
+
+	if (ac == 1)
+	{
+		printf("\n");
+		return 0;
+	}
+	i = 1;
+	while (i < ac)
+	{
+		if (i == 1)
+		{
+			if (strncmp(av[i], "-n", 3) == 0)
+			{
+				nl = NO;
+				i++;
+				continue ;
+			}
+			else
+				nl = YES;
+		}
+		printf ("%s", av[i]);
+		i++;
+		if (i < ac)
+			printf(" ");
+	}
+	if (nl == YES)
+		printf("\n");
+	return (0);
 }
